fd94e0a988f645e64c7834ab96ca53b4eb15c252
EliaPennoni <elia.pennoni1991@gmail.com>
2024-10-15 16:55:32 +0200

    esercizio terminato

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about</title>
+</head>
+<body>
+    <h1>about laravel</h1>
+    <a href="/">HOME</a>
+    <a href="{{route('about')}}">ABOUT</a>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel hello world</title>
+</head>
+<body>
+    laravel hello world
+
+    <a href="/about">ABOUT</a>
+    <a href="{{route('home')}}">HOME</a>
+</body>
+</html>
